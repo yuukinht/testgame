e6a0311b7b269adbe7dda32440f32a2219632ce2
yuukinht <50871323+yuukinht@users.noreply.github.com>
2020-01-20 14:27:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
 var express = require('express');
 var app = express();
 var server = app.listen(process.env.PORT || 3000);
-
-app.use(express.static('public'));
 var io = require('socket.io')(server);
 io.sockets.on('connection',
   function (socket) { 
-	socket.on('setUsername', function(data) {		
-		console.log(data.name + " Join!");		
-	});
-	socket.on('other', function(data) {
-		socket.broadcast.emit('other',{id:socket.id,data:data});
-    });	
-	socket.on('otherbullet', function(data) {
-		socket.broadcast.emit('otherbullet',{id:socket.id,data:data});
-    });	
-    socket.on('disconnect', function() {
-		socket.broadcast.emit('logout',{id:socket.id});
+	socket.on('mess', function(data) {
+		socket.broadcast.emit('other',data);
     });	
   }
 );
